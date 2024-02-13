2d86be0d97b5b8a3525b85288a58afa18ea79f42
Andrey Kochetov <and-usmail-586@yandex.ru>
2024-02-13 10:46:00 +0500

    Include NUnit

---
+++ Task_10.Tests/MainLogicControllerTests.cs
@@ -0,0 +1,16 @@
+namespace Task_10.Tests
+{
+    public class MainLogicControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
