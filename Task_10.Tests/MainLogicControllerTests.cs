03e2214bc2c7703dc55610ec4a11a9619ba5bf57
Andrey Kochetov <and-usmail-586@yandex.ru>
2024-02-14 17:59:45 +0500

    NUnit for Task 3

--- Task_10.Tests/MainLogicControllerTests.cs
+++ Task_10.Tests/MainLogicControllerTests.cs
@@ -28,11 +28,38 @@ namespace Task_10.Tests
             Assert.That(result, Is.EqualTo(actual));
         }
 
+        private static IEnumerable<object[]> Task3TestData()
+        {
+            yield return new object[]
+            {
+                "abcddf",
+                new Dictionary<char, int>
+                {
+                    ['a'] = 1,
+                    ['b'] = 1,
+                    ['c'] = 1,
+                    ['d'] = 2,
+                    ['f'] = 1
+                }
+            };
+            yield return new object[]
+            {
+                "cbefddca",
+                new Dictionary<char, int>
+                {
+                    ['c'] = 2,
+                    ['b'] = 1,
+                    ['e'] = 1,
+                    ['f'] = 1,
+                    ['d'] = 2,
+                    ['a'] = 1
+                }
+            };
+        }
 
-        //[TestCase("cbafed",)]
+        [TestCaseSource(nameof(Task3TestData))]
         public void LogicTask3_abcddf_a1b1c1d2f1return(string inputLine, Dictionary<char, int> result)
         {
-            new Dictionary<char, int> { { 'a', 1 }, { 'b', 1 } };
             Dictionary<char, int> actual = mainLogic.LogicTask3(inputLine);
             Assert.That(result, Is.EqualTo(actual));
         }
@@ -50,7 +77,7 @@ namespace Task_10.Tests
         [TestCase("bcda", 2)]
         [TestCase("fedabc", 1)]
         [TestCase("fedabc", 2)]
-        public void LogicTask4_bcda_abcd(string inputLine, int sortOption)
+        public void LogicTask5_bcda_abcd(string inputLine, int sortOption)
         {
             string actual = mainLogic.LogicTask5(inputLine, sortOption);
             char[] result = inputLine.ToCharArray();
